0cd5876a3bccf9b4245b6ac6c2b2f7ca4e9c066e
Sebastian <sebastian@iamsebastian.dev>
2022-07-10 10:16:21 +0200

    feat: improved error handling

--- bin/index.mjs
+++ bin/index.mjs
@@ -8,17 +8,27 @@ import { mergeConfig } from '../lib/config/mergeConfig.mjs';
 import { server } from '../lib/http/server.mjs';
 import { fileWatcher } from '../lib/utils/fileWatcher.mjs';
 
+/**
+ * This is the main executable function for byndly. The method will get or create a
+ * complete config object, start a server to serve the files and create a file watcher
+ * to enable reloading the frontend when the file indicating the bundle changes.
+ */
+
 (async (args) => {
     // merge all available config forms and create the config object
     const config = mergeConfig(await getConfig(args.config || args.c), args);
 
     // create the minimal server that serves the bundle, includes and the reload event
-    const srv = server(config);
-    srv.serve();
+    try {
+        const srv = server(config);
+        srv.serve();
 
-    // initialize the fileWatcher if enabled, to reload the server when the bundle changes.
-    config.watch && fileWatcher(config.bundle, srv.reload);
+        // initialize the fileWatcher if enabled, to reload the server when the bundle changes.
+        config.watch && fileWatcher(config.bundle, srv.reload);
 
-    !config.quiet &&
-        console.log(`\x1b[32m@Byndly: 🚀 Bundle now available at 'http://${config.host}:${config.port}'...\x1b[0m`);
+        !config.quiet &&
+            console.log(`\x1b[32m@Byndly: 🚀 Bundle now available at 'http://${config.host}:${config.port}'...\x1b[0m`);
+    } catch (e) {
+        throw new Error(`@Byndly: ${e}`);
+    }
 })(getEnvArgs());
