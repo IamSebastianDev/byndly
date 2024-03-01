06ddb84020f271ff9e1db5a4375848ab3b23795a
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-01 17:11:15 +0100

    feat: added config parsing system

---
+++ src/lib/config/validate-config.ts
@@ -0,0 +1,44 @@
+/** @format */
+
+import { ByndlyConfig } from '../types/byndly-config.type';
+import { EnvArgs } from '../types/env-args.type';
+
+const defaultConfig = {
+    module: false,
+    watch: false,
+    port: 31415,
+    host: '127.0.0.1',
+    quiet: false,
+    verbose: false,
+    name: 'Byndly',
+    bootstrap: undefined,
+    include: undefined,
+    bundle: undefined,
+    template: undefined,
+};
+
+const ARGS_DICT = {
+    w: 'watch',
+    p: 'port',
+    h: 'host',
+    q: 'quiet',
+    v: 'verbose',
+    m: 'module',
+    b: 'bundle',
+    t: 'template',
+};
+
+export const validateConfig = (
+    pkg: Record<PropertyKey, unknown>,
+    env: EnvArgs,
+    config: Record<PropertyKey, unknown>,
+): ByndlyConfig => {
+    return {
+        ...defaultConfig,
+        module: pkg.type === 'module',
+        name: pkg.name ?? 'Byndly',
+        bundle: pkg.type === 'module' ? pkg.module ?? pkg.main : pkg.main,
+        ...Object.fromEntries(Object.entries(env).map(([key, value]) => [ARGS_DICT[key] ?? key, value])),
+        ...config,
+    } satisfies ByndlyConfig;
+};
