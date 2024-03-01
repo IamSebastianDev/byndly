a930cef8aae13ff29d64a12db051c1665d69e80a
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-01 17:04:31 +0100

    chore: adapt build process

---
+++ .config/rollup.config.js
@@ -0,0 +1,53 @@
+/** @format */
+
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+import cleanup from 'rollup-plugin-cleanup';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import pkg from '../package.json' assert { type: 'json' };
+import shebang from 'rollup-plugin-add-shebang';
+import json from '@rollup/plugin-json';
+
+const bundle = (config) => ({
+    input: './src/index.ts',
+    external: (id) => !/^[./]/.test(id),
+    ...config,
+});
+
+export default [
+    bundle({
+        plugins: [commonjs(), resolve(), esbuild(), json(), cleanup({ extensions: ['ts'] })],
+        output: [
+            {
+                file: pkg.main,
+                format: 'cjs',
+                sourcemap: true,
+            },
+            {
+                file: pkg.module,
+                format: 'es',
+                sourcemap: true,
+            },
+        ],
+    }),
+    bundle({
+        input: './src/bin/index.ts',
+        plugins: [commonjs(), resolve(), esbuild(), json(), cleanup({ extensions: ['ts'] })],
+        output: [
+            {
+                format: 'es',
+                sourcemap: false,
+                plugins: [shebang({ include: `**/*.cjs` })],
+                file: pkg.bin,
+            },
+        ],
+    }),
+    bundle({
+        output: {
+            file: pkg.types,
+            format: 'es',
+        },
+        plugins: [resolve(), commonjs(), cleanup({ extensions: ['.ts'] }), dts()],
+    }),
+];
