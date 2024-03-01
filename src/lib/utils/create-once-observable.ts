c399f7b0ece4b73037cb631872bdbce5fb8ab06e
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-01 22:16:17 +0100

    feat: create once observable

---
+++ src/lib/utils/create-once-observable.ts
@@ -0,0 +1,9 @@
+/** @format */
+
+export const createOnceObservable = () => {
+    let consumer: null | (() => void) = null;
+    return {
+        emit: () => consumer?.(),
+        subscribe: (subscriber: () => void) => (consumer = subscriber),
+    };
+};
