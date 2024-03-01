0c7bbd0417bb9b6185e16b2f61fd463df500f1fa
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-01 22:32:32 +0100

    feat: implement basic server system

--- src/lib/byndly.ts
+++ src/lib/byndly.ts
@@ -1,27 +1,58 @@
 /** @format */
 
+import { server } from '../../.idea/http/server.mjs';
 import { checkConfigMatch } from './config/check-config-match';
+import { httpServer } from './http/server';
 import { ByndlyConfig } from './types/byndly-config.type';
+import { createFileWatcher } from './utils/create-file-watcher';
 import { format } from './utils/format';
+import { onShutdown } from './utils/on-shutdown';
 
-const c = (a: ByndlyConfig) => JSON.stringify(a);
+export const byndly = (config: Required<ByndlyConfig>) => {
+    let _config: Required<ByndlyConfig> = config;
+    if (!_config.bundle) {
+        throw new Error(
+            `No bundle to serve. Define one in your config file or pass it via the --b or --bundle flag. Ensure the path is correct.`,
+        );
+    }
 
-export const byndly = (config: ByndlyConfig) => {
-    let _config: ByndlyConfig = config;
-    // @todo -> create server implementation
-    const server = {};
+    const server = httpServer();
+    let bundleWatcher = createFileWatcher(_config.bundle);
+    const unsubscribe = bundleWatcher.subscribe(async () => {
+        server.reload();
+    });
+
+    onShutdown(async () => {
+        bundleWatcher.close();
+        unsubscribe();
+    });
 
     const runInit = () => {
-        // @todo -> Run the server setup, bundle watcher and other processes.
+        if (!_config.bundle) {
+            throw new Error(
+                `No bundle to serve. Define one in your config file or pass it via the --b or --bundle flag. Ensure the path is correct.`,
+            );
+        }
+
+        if (bundleWatcher) {
+            bundleWatcher.close();
+            bundleWatcher = createFileWatcher(_config.bundle);
+        }
+
+        server.close();
+        server.listen(_config);
     };
 
+    runInit();
+
     return {
-        setConfig: (config: ByndlyConfig) => {
+        setConfig: (config: Required<ByndlyConfig>) => {
             // Return early if the config matches the previous config;
             if (checkConfigMatch(_config, config)) return;
 
             // Run the init method when the config has changed.
-            console.log(format.info('Config changed, reloading...'));
+            console.clear();
+            console.log(format.info(`[${new Date().toLocaleTimeString()}] Config change detected, restarting! 🚀`));
             _config = config;
             runInit();
         },
---
+++ src/lib/http/server.ts
@@ -0,0 +1,53 @@
+/** @format */
+
+import { IncomingMessage, Server, ServerResponse, createServer } from 'node:http';
+import { ByndlyConfig } from '../types/byndly-config.type';
+import { createOnceObservable } from '../utils/create-once-observable';
+import { format } from '../utils/format';
+import { onShutdown } from '../utils/on-shutdown';
+import { requestHandler } from './request-handler';
+
+export const httpServer = () => {
+    let server: Server | null = null;
+    let config: ByndlyConfig | null = null;
+
+    const ping = createOnceObservable();
+
+    // Method to start the server using the handler function
+    const listen = (_config: ByndlyConfig) => {
+        config = _config;
+
+        if (server) return;
+        server = createServer(requestHandler(config, ping));
+
+        const { port, host, quiet } = config;
+
+        if (port && host) {
+            server.listen(+port, host);
+            if (!quiet) {
+                console.log(format.info(`Bundle now available at http://${host}:${port} 📦`));
+            }
+            return;
+        }
+
+        return close();
+    };
+
+    // Method to shut down the server and remove all existing connections
+    const close = () => {
+        if (!server) return;
+        if (server.listening) {
+            server.closeAllConnections();
+            server.close();
+            server = null;
+        }
+    };
+
+    onShutdown(async () => close());
+
+    return {
+        listen,
+        close,
+        reload: () => ping.emit(),
+    };
+};
