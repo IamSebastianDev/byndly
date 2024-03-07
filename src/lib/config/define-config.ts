36548f6e0e66512bab838f37212fe2f5f6159e01
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-03-07 21:08:25 +0100

    docs: add documentation to public functions

--- src/lib/config/define-config.ts
+++ src/lib/config/define-config.ts
@@ -2,4 +2,18 @@
 
 import { ByndlyConfig } from '../types/byndly-config.type';
 
+/**
+ * This function is exposed by byndly to simplify config creation. Use it to create and export the config.
+ *
+ * ```ts
+ * import {defineConfig} from "byndly";
+ *
+ * export default defineConfig({
+ *   // ...config properties
+ * })
+ * ```
+ *
+ * @param {Partial<ByndlyConfig>} config - The configuration object provided to the function.
+ * @returns
+ */
 export const defineConfig = (config: Partial<ByndlyConfig>) => config;
